c56ec162990510470e3979eb77de0ef00ae5822e
Iliass KAHLI <iliass.kahli.01@gmail.com>
2017-02-28 00:11:02 +0100

    afficher notices

--- Biblio/biblio/src/main/java/model/Oeuvre.java
+++ Biblio/biblio/src/main/java/model/Oeuvre.java
@@ -1,68 +1,41 @@
 package model;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
-public class Oeuvre
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Oeuvre")
+public class Oeuvre implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 
-	//@Id @GeneratedValue
-	//@Column(name = "oeuvre_id")
+	@Id @GeneratedValue
+	@Column(name = "oeuvre_id")
 	private int id;
 	
-	//@Column(name = "titre")
-	private String titre;
+	@Column(name = "titre_oeuvre")
+	private String titreOeuvre;
 	
-	//@Column(name = "forme")
+	@Column(name = "forme")
 	private String forme;
 	
-	//@Column(name = "date_creation")
+	@Column(name = "date_creation")
 	private Date dateCreation;
 	
-	//@Column(name = "autre_caracteristique")
-	private String autreCaracteristique;
-	
-	//@Column(name = "completude_visee")
-	private String completudeVisee;
-	
-	//@Column(name = "public_vise")
-	private String publicVise;
-	
-	//@Column(name = "contexte")
-	private String contexte;
-	
-	//@Column(name = "distribution_execution")
-	private String distributionExecution; // oeuvre musicale
-	
-	//@Column(name = "reference_numerique")
-	private String referenceNumerique; // oeuvre musicale
-	
-	//@Column(name = "tonalite")
-	private String tonalite; // oeure musicale
-	
-	//@Column(name = "coordonnees")
-	private String coordonnees; // oeuvre cartographique
-	
-	//@Column(name = "equinoxe")
-	private String equinoxe; // oeuvre cartographique
-	
-	//@OneToMany(mappedBy = "oeuvre")
+	@OneToMany(mappedBy = "oeuvre")
 	private List<Expression> expressions;
 	
-	//@OneToMany(mappedBy = "oeuvre")
+	@OneToMany(mappedBy = "oeuvre")
 	private List<Personne> personnes;
 	
-	//@OneToMany(mappedBy = "oeuvre")
+	@OneToMany(mappedBy = "oeuvre")
 	private List<Collectivite> collectivites;
-
-	public Oeuvre(int id, String titre, String forme, Date dateCreation) {
-		super();
-		this.id = id;
-		this.titre = titre;
-		this.forme = forme;
-		this.dateCreation = dateCreation;
-	}
-	
-	
 }
